08485b1b902b9c2d47dda3c3b45d71e136f380be
Adriano Matias <adriano.matias04@hotmail.com>
2023-12-28 17:11:28 -0300

    commit

---
+++ trilha-net-testes-unitarios-desafio-main/TestesUnitarios.Desafio.Console/Services/ValidacoesLista.cs
@@ -0,0 +1,45 @@
+namespace TestesUnitarios.Desafio.Console.Services
+{
+    public class ValidacoesLista
+    {
+        public List<int> MultiplicarElementosPor2(List<int> lista)
+        {
+            List<int> resultado = new List<int>();
+
+            foreach (int numero in lista)
+            {
+                resultado.Add(numero * 2);
+            }
+
+            return resultado;
+        }
+
+        public List<int> RemoverNumerosNegativos(List<int> lista)
+        {
+            var listaSemNegativos = lista.Where(x => x > 0);
+            return listaSemNegativos.ToList();
+        }
+
+        public bool ListaContemDeterminadoNumero(List<int> lista, int numero)
+        {
+            var contem = lista.Contains(numero);
+            return contem;
+        }
+
+        public List<int> MultiplicarNumerosLista(List<int> lista, int numero)
+        {
+            var listaMultiplicada = lista.Select(x => x * numero).ToList();
+            return listaMultiplicada;
+        }
+
+        public int RetornarMaiorNumeroLista(List<int> lista)
+        {
+            return lista.Max();
+        }
+
+        public int RetornarMenorNumeroLista(List<int> lista)
+        {
+            return lista.Min();
+        }
+    }
+}
